071e90df4a1ad5b09fe641eb6b85439dfbc8ebb5
imroysakshi <roysakshi355@gmail.com>
2026-01-23 18:19:45 +0530

    example 5 modifies

--- src/features/order/example5.ts
+++ src/features/order/example5.ts
@@ -4,3 +4,10 @@ export function example5() {
     message: "Example5 function executed successfully"
   };
 }
+
+export function example5Desc() {
+  return {
+    success: true,
+    message: "Example5 description function executed successfully"
+  };
+}
